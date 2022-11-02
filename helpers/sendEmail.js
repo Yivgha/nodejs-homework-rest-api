9f49e18c830e032196f9e2e05e84b472f14c9062
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2022-11-02 23:37:29 +0200

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -11,20 +11,4 @@ const sendEmail = async (data) => {
     return true;
 };
 
-// const msg = {
-//     from: "testusermail@meta.ua",
-//     to: "geniukne@gmail.com",
-//     subject: 'Sending with SendGrid is Fun',
-//     // text: 'and easy to do anywhere, even with Node.js',
-//     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// }
-// sgMail
-//   .send(msg)
-//   .then(() => {
-//     console.log('Email sent')
-//   })
-//   .catch((error) => {
-//     console.error(error)
-//   })
-
 module.exports = sendEmail;
